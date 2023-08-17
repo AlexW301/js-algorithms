756b47ffbdbed3ece0c56ab6d53262370606613c
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-08-17 15:55:25 -0400

    Update collectOddValues.js

--- Section 7 - Recursion/collectOddValues.js
+++ Section 7 - Recursion/collectOddValues.js
@@ -1,3 +1,5 @@
+// Helper Method Recursion
+
 function collectOddValues(arr) {
     let result = [];
 
@@ -17,3 +19,20 @@ function collectOddValues(arr) {
 }
 
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+// Pure Recursion
+
+function collectOddValuesPure(arr) {
+    let newArr = [];
+
+    if(arr.length === 0) return newArr;
+
+    if(arr[0] % 2 !== 0) {
+        newArr.push(arr[0]);
+    }
+
+    newArr = newArr.concat(collectOddValuesPure(arr.slice(1)))
+    return newArr;
+}
+
+console.log(collectOddValuesPure([1, 2, 3, 4, 5, 6, 7, 8, 9]))
